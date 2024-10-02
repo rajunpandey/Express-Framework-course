b4a3b02abc6711da174ed1829fa29e8cea648edc
rajun <rajunpandey1234@gmail.com>
2024-10-02 13:45:00 +0545

    learned middleware

--- server2.js
+++ server2.js
@@ -5,7 +5,6 @@ const path = require('path');
 const PORT = process.env.PORT || 3500;
 
 app.get('^/$|/index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname });
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
