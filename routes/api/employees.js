79b303dcff4e3ef11f27b225866f8264679ce286
rajun <rajunpandey1234@gmail.com>
2024-10-10 14:03:30 +0545

    learned Authorization

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -35,13 +35,15 @@ module.exports = router;
 const express = require('express');
 const router = express.Router();
 const employeesController = require('../../controllers/employeesController');
-const verify = require('../../middleware/verifyJWT');
+const ROLES_LIST = require('../../config/roles_list');
+//const verify = require('../../middleware/verifyJWT');
+const verifyRoles = require('../../middleware/verifyRoles');
 
 router.route('/')
     .get(employeesController.getAllEmployees)
-    .post(employeesController.createNewEmployee)
-    .put(employeesController.updateEmployee)
-    .delete(employeesController.deleteEmployee);
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.createNewEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeesController.updateEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeesController.deleteEmployee);
 
 router.route('/:id')
     .get(employeesController.getEmployee);
