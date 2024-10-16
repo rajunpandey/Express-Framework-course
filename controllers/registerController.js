a3d713f7468499cc0bbba2675105b5e12ecb1c90
rajun <rajunpandey1234@gmail.com>
2024-10-16 09:59:28 +0545

    learned Mongoose Data Models

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,4 +1,4 @@
-const usersDB = {
+/* const usersDB = {
     users: require('../model/users.json'),
     setUsers: function (data) { this.users = data }
 }
@@ -27,6 +27,37 @@ const handleNewUser = async (req, res) => {
             JSON.stringify(usersDB.users)
         );
         console.log(usersDB.users);
+        res.status(201).json({ 'success': `New user ${user} created!` });
+    } catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
+}
+
+module.exports = { handleNewUser }; */
+
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+
+const handleNewUser = async (req, res) => {
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+
+    // check for duplicate usernames in the db
+    const duplicate = await User.findOne({ username: user }).exec();
+    if (duplicate) return res.sendStatus(409); //Conflict 
+
+    try {
+        //encrypt the password
+        const hashedPwd = await bcrypt.hash(pwd, 10);
+
+        //create and store the new user
+        const result = await User.create({
+            "username": user,
+            "password": hashedPwd
+        });
+
+        console.log(result);
+
         res.status(201).json({ 'success': `New user ${user} created!` });
     } catch (err) {
         res.status(500).json({ 'message': err.message });
